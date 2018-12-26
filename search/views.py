9495b3a49155e6b703c7dc46b9d9d449b44de63c
sagarpawardev <sagar.pawar.dev@gmail.com>
2018-12-26 08:02:12 +0530

    Initial Message

--- search/views.py
+++ search/views.py
@@ -120,7 +120,6 @@ def city_search(request):
 @api_view(["GET"])
 def advance_search(request):
     if request.method == 'GET':
-
         # Handle Request Params
         mark = request.query_params.get('mark', None)
         city = request.query_params.get('city', None)
@@ -128,20 +127,55 @@ def advance_search(request):
         college_name = request.query_params.get('college', None)
 
         # Query DB
-        queryset = CollegeResult.objects.all()
-        if mark is not None:
-            queryset = queryset.filter(stage_mark__lte=mark)
-        if city is not None:
-            queryset = queryset.filter(city__icontains=city)
-        if branch_name is not None:
-            queryset = queryset.filter(branch_name__icontains=branch_name)
-        if college_name is not None:
-            queryset = queryset.filter(college_name__icontains=college_name)
+        # queryset = CollegeResult.objects.all()
 
-        # Apply Pagination
-        pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
-        paginator = pagination_class()
-        page = paginator.paginate_queryset(queryset, request)
-        serializer = CollegeResultSerializer(page, many=True)
+        query = 'SELECT a.college_code college_code, ' + \
+                'a.branch_name branch_name, ' + \
+                'a.college_name college_name, ' + \
+                'max_cutoff_rank stage_rank, ' + \
+                'cutoff_type cutoff_type, ' + \
+                'a.stage_mark stage_mark, ' + \
+                'a.id id ' + \
+                'FROM   vw_college_result a, ' + \
+                '   (SELECT college_code, ' + \
+                '           branch_name, ' + \
+                '           Min(stage_rank) max_cutoff_rank ' + \
+                '       FROM  ( ' + \
+                '			    SELECT college_code, ' + \
+                '                   branch_name, ' + \
+                '                   stage_rank ' + \
+                '			    FROM   vw_college_result ' + \
+                '			    WHERE  cutoff_type IN( "AI" ) ' + \
+                '		    ) ' + \
+                '        GROUP BY college_code, branch_name' \
+                '       ) b ' + \
+                'WHERE  a.college_code = b.college_code ' + \
+                '   AND a.branch_name = b.branch_name ' + \
+                '   AND a.stage_rank = max_cutoff_rank ' + \
+                '   AND cutoff_type IN( "AI" ) ' + \
+                'ORDER  BY a.college_code, ' + \
+                '           a.branch_name '
+    print(query)
 
-        return paginator.get_paginated_response(serializer.data)
+    queryset = CollegeResult.objects.raw(query)
+
+    # queryset = CollegeResult.objects.raw(' SELECT * from vw_college_result')
+
+    queryset = list(queryset)
+
+    if mark is not None:
+        queryset = queryset.filter(stage_mark__lte=mark)
+    if city is not None:
+        queryset = queryset.filter(city__icontains=city)
+    if branch_name is not None:
+        queryset = queryset.filter(branch_name__icontains=branch_name)
+    if college_name is not None:
+        queryset = queryset.filter(college_name__icontains=college_name)
+
+    # Apply Pagination
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+    paginator = pagination_class()
+    page = paginator.paginate_queryset(queryset, request)
+    serializer = CollegeResultSerializer(page, many=True)
+
+    return paginator.get_paginated_response(serializer.data)
